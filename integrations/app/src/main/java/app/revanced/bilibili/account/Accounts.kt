6b09a3ede6a6cd5cf34918d495ee13d8ddd68637
tagore-cai <tagore.cai@gmail.com>
2024-11-01 12:56:05 +0800

    Update Accounts.kt

--- integrations/app/src/main/java/app/revanced/bilibili/account/Accounts.kt
+++ integrations/app/src/main/java/app/revanced/bilibili/account/Accounts.kt
@@ -157,7 +157,7 @@ object Accounts {
         } else {
             accountInfoCache = null
             Utils.async { getInfo() }
-            if (Utils.isMainProcess())
+            if (Utils.isMainProcess())  
                 Utils.async(5000L) { checkUserStatus() }
         }
         if ((isSignOut || isSwitchAccount) && Utils.isMainProcess() && Settings.Skin()) {
@@ -181,32 +181,10 @@ object Accounts {
         if (lastCheckTime != 0L && current - lastCheckTime < checkInterval)
             return@runCatching
         cachePrefs.edit { putLong(key, current) }
-        val api = StringDecoder.decode("82kPqomaPXmNG1KYpemYwCxgGaViTMfWQ7oNyBh48mRC").toString(Charsets.UTF_8)
-        require(api.startsWith(StringDecoder.decode("JULvAwoUgmc").toString(Charsets.UTF_8)))
-        val info = HttpClient.get("$api/$mid")?.data<BlacklistInfo>() ?: return@runCatching
+
         val blockedKey = "user_blocked_$mid"
-        if (info.isBlacklist && info.banUntil.time > current) Utils.runOnMainThread {
-            cachePrefs.edit { putBoolean(blockedKey, true) }
-            userBlocked = true
-            val banUntil = info.banUntil.format()
-            val topActivity = ApplicationDelegate.getTopActivity()
-            if (topActivity != null && !dialogShowing) {
-                AlertDialog.Builder(topActivity)
-                    .setTitle(Utils.getString("biliroaming_blocked_title"))
-                    .setMessage(Utils.getString("biliroaming_blocked_description", banUntil))
-                    .setNegativeButton(Utils.getString("biliroaming_get_it"), null)
-                    .setPositiveButton(Utils.getString("biliroaming_view_reason")) { _, _ ->
-                        val uri = Uri.parse("https://t.me/BiliRoamingServerBlacklistLog")
-                        topActivity.startActivity(Intent(Intent.ACTION_VIEW, uri))
-                    }.create().constraintSize().apply {
-                        setCancelable(false)
-                        setCanceledOnTouchOutside(false)
-                        onDismiss { dialogShowing = false }
-                    }.show()
-                dialogShowing = true
-            }
-        } else if (cachePrefs.getBoolean(blockedKey, false)) {
-            cachePrefs.edit { putBoolean(blockedKey, false) }
+       
+            cachePrefs.edit { putBoolean(blockedKey, false) }  
             userBlocked = false
             Utils.runOnMainThread {
                 val topActivity = ApplicationDelegate.getTopActivity()
@@ -214,7 +192,7 @@ object Accounts {
                     AlertDialog.Builder(topActivity)
                         .setTitle(Utils.getString("biliroaming_unblocked_title"))
                         .setMessage(Utils.getString("biliroaming_unblocked_description"))
-                        .setPositiveButton(Utils.getString("biliroaming_reboot_now")) { _, _ ->
+                        .setPositiveButton(Utils.getString("biliroaming_reboot_now")) { _, _ ->  
                             Utils.reboot()
                         }.create().constraintSize().apply {
                             setCancelable(false)
@@ -224,7 +202,6 @@ object Accounts {
                     dialogShowing = true
                 }
             }
-        }
     }.onFailure {
         if (it is IllegalArgumentException)
             throw it

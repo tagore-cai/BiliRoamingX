62480e9258dee300a0caa56feb93948c13bfe5a2
tagore-cai <tagore.cai@gmail.com>
2024-11-01 13:18:37 +0800

    Update ReplyAdd.kt

--- integrations/app/src/main/java/app/revanced/bilibili/patches/okhttp/hooks/ReplyAdd.kt
+++ integrations/app/src/main/java/app/revanced/bilibili/patches/okhttp/hooks/ReplyAdd.kt
@@ -2,7 +2,6 @@ package app.revanced.bilibili.patches.okhttp.hooks
 
 import app.revanced.bilibili.patches.okhttp.ApiHook
 import app.revanced.bilibili.settings.Settings
-import app.revanced.bilibili.utils.CommentChecker
 import app.revanced.bilibili.utils.forEach
 import app.revanced.bilibili.utils.orEmpty
 import app.revanced.bilibili.utils.toJSONObject
@@ -19,14 +18,11 @@ object ReplyAdd : ApiHook() {
             return response
         val reply = data.optJSONObject("reply")
             ?: return response
-        val oid = reply.optLong("oid")
-        val replyId = reply.optLong("rpid")
         val content = reply.optJSONObject("content")
             ?: return response
         val message = content.optString("message")
         val pictures = content.optJSONArray("pictures")
         val hasPicture = pictures.orEmpty().length() > 0
-        val finalMessage = "$message ${if (hasPicture) " [带图]" else ""}".trim()
         if (Settings.UnlockGif()) {
             content.put("picture_scale", 1.5)
             pictures?.forEach { p ->
